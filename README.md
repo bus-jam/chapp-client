7aebca0b6a80540b94fb54cabad797669de4c988
Matt Herriges <herrigesmt@gmail.com>
2020-09-04 16:26:30 -0700

    got chat functionality up and running

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
+```
+ _____ _                       
+/  __ \ |                      
+| /  \/ |__   __ _ _ __  _ __  
+| |   | '_ \ / _` | '_ \| '_ \ 
+| \__/\ | | | (_| | |_) | |_) |
+ \____/_| |_|\__,_| .__/| .__/ 
+                  | |   | |    
+                  |_|   |_|  
+```
+
 [![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://github.com/bus-jam/chapp-client/blob/staging/license)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Goal
 
 
-# chapp-client
+## Installation
+
+## Usage
+
+## The Team
+
+* [Matt Herriges]()
+* [Jennifer Chinzi](https://github.com/jchinzi)
+* [Sean Murray](https://github.com/seanjmurray)
+* [Blake Romero](https://github.com/blakerom)
+
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const repl = require('repl');
+const chalk = require('chalk');
+// FROM https://blog.bitsrc.io/build-a-command-line-real-time-chat-app-using-socketio-f2e3553d6228
+const socket = require('socket.io-client')('http://localhost:3002');
+let os = require('os')
+let username = null;
+
+
+socket.on('disconnect', () => {
+    socket.emit('disconnect');
+});
+
+socket.on('connect', () => {
+    username = os.userInfo().username || 'guest';
+    console.log(chalk.red(`=== start chatting ${username} ===`));
+});
+
+socket.on('message', data => {
+    const { cmd, username } = data;
+    console.log(chalk.green(`${username}: ${cmd.split('\n')[0]}`));
+});
+
+socket.on('toxic', data => {
+  const { cmd } = data;
+  console.log(chalk.red(`SERVER: ${cmd.split('\n')[0]}`))
+})
+
+repl.start({
+    prompt: '',
+    terminal: false,
+    eval: (cmd) => {
+          socket.send({cmd, username});
+    
+    }
+  
+});
+
