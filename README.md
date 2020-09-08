4238412134d08865a42e362bce00e69b3c094b35
Matt Herriges <herrigesmt@gmail.com>
2020-09-07 17:11:38 -0700

    day one commit, added funcitonality towards mvp including database, rooms, whispers, and more

--- README.md
+++ README.md
@@ -9,21 +9,37 @@
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-brightgreen.svg)](https://github.com/bus-jam/chapp-client/blob/staging/license)
 [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
-[![Version](https://img.shields.io/badge/Version-0.1.0-brightgreen.svg)](https://github.com/bus-jam/chapp-client#readme)
+[![Version](https://img.shields.io/badge/Version-0.2.0-brightgreen.svg)](https://github.com/bus-jam/chapp-client#readme)
 
 ## Goal
-We wanted to build a socket io driven command line chat app. Our server uses **TensorFlow** to moderate messages and try to stop toxicity and bad language. 
+
+We wanted to build a socket io driven command line chat app. Our server uses **TensorFlow** to moderate messages and try to stop toxicity and bad language.
 
 ## Installation
 
 ## Usage
 
+    /join <room>
+
+Joins specified room.
+
+    /whisper <username> <message>
+
+Sends a private message to the specified user.
+
+    /exit
+
+Exits the app and disconects from server.
+
 ## The Team
 
-* [Matt Herriges]()
+* [Matt Herriges](https://github.com/herrigesmt)
 * [Jennifer Chinzi](https://github.com/jchinzi)
 * [Sean Murray](https://github.com/seanjmurray)
 * [Blake Romero](https://github.com/blakerom)
 
+## Version History
 
-- 2020-09-04 V 0.1.0  Initial files uploaded, basic app functionality added.
+* 2020-09-04 V 0.1.0 Initial files uploaded, basic app functionality added.
+* 2020-09-07 V 0.2.0 Add basic menu and promts, handle sign-in/up events and saving users to database, private messages, and room switching.
+
--- index.js
+++ index.js
@@ -1,105 +1,164 @@
-'use strict';
+'use strict'
+// ================================================================
+// Required Libraries
+const chalk = require('chalk')
+const socket = require('socket.io-client')('http://localhost:3002')
+const inquirer = require('inquirer')
 
-// const repl = require('repl');
-const chalk = require('chalk');
-// FROM https://blog.bitsrc.io/build-a-command-line-real-time-chat-app-using-socketio-f2e3553d6228
-const socket = require('socket.io-client')('http://localhost:3002');
-let os = require('os')
-let username = null;
-const readline = require('readline');
-const inquirer = require('inquirer');
-const repl = require('repl');
+// ================================================================
+// Establish Objects
 
-const ui = new inquirer.ui.BottomBar();
-const rl = readline.createInterface(process.stdin, process.stdout)
+// TODO: maybe modularize these question objects and require them in to clean this up
+
 const login = {
   type: 'list',
   name: 'answer',
   message: 'Sign-in or sign-up',
-  choices: ['sign-in','sign-up']
+  choices: ['sign-in', 'sign-up']
 }
 
 const signinUsername = {
   type: 'input',
   name: 'username',
-  message: 'Enter your username',
+  message: 'Enter your username'
 }
 
 const signinPassword = {
   type: 'password',
   name: 'password',
-  message: 'Enter your password',
+  message: 'Enter your password'
 }
 
 const menu = {
   type: 'list',
   name: 'room',
-  choices: [
-    
-  ]// socket.emit('getrooms'),
+  choices: []// socket.emit('getrooms'),
 }
 
 const message = {
   type: 'input',
-  name: 'cmd',
-  message: '',
+  name: 'chat',
+  message: ''
 }
 
+// ================================================================
+// Disconnect Event
+
 socket.on('disconnect', () => {
-    socket.emit('disconnect');
-});
+  socket.emit('disconnect')
+})
 
-socket.on('connect', () => {
-    username = os.userInfo().username || 'guest';
-    inquirer.prompt([login]).then(answers => {
-      if(answers.answer === 'sign-in'){
-        inquirer.prompt([signinUsername,signinPassword]).then(user => {
-          socket.emit('signin', user);
-          ui.log.write(chalk.red(`=== start chatting ${user.username} ===`));
-          inquirer.prompt(message).then(message => {
-            console.log('i am here', message);
-            const { cmd } = message;
-            socket.send({cmd, username:'null'})
-          });
-          
-        });
-      } else {
-        inquirer.prompt([signinUsername,signinPassword]).then(user => {
-          socket.emit('signup', user);
-        });
-      }
-    });
-});
+socket.on('connect', loginOrSignup)
 
-socket.on('invalid-login', () => {
-  // populate me later
-});
+function loginOrSignup () {
+  inquirer.prompt([login]).then(answers => {
+    if (answers.answer === 'sign-in') {
+      inquirer.prompt([signinUsername, signinPassword]).then(user => {
+        socket.emit('signin', user)
+      })
+    } else {
+      inquirer.prompt([signinUsername, signinPassword]).then(user => {
+        socket.emit('signup', user)
+      })
+    }
+  })
+}
 
+function listenForChat () {
+  inquirer.prompt(message).then(message => {
+    const cmd = message.chat
+    if (cmd[0] === '/') {
+      handleCommand(cmd)
+    } else {
+      socket.send({ cmd, username: socket.username })
+      listenForChat()
+    }
+  })
+}
+
+// ================================================================
+// Invalid Login Response - Loop back to login/signup prompt
+
+socket.on('invalid-login', (error) => {
+  console.log(chalk.red(`SERVER ERROR: ${error.error}`))
+  loginOrSignup()
+})
+
+// ================================================================
+// After Successful Login
 
 socket.on('connected', (username) => {
-  ui.log.write(chalk.red(`=== start chatting ${username} ===`));
-});
+  socket.username = username // TODO: is this reduntant?
+  console.log(chalk.red(`=== start chatting ${username} ===`))
+  listenForChat()
+})
 
-socket.on('join', (user, room) => {
-  console.log(chalk.red(`${user} joined the room ${room}`));
-});
+// ================================================================
+// Joined Event
+
+socket.on('joined', (room) => {
+  listenForChat()
+})
+
+// ================================================================
+// Normal Message Response
 
 socket.on('message', data => {
-    const { cmd, username } = data;
-    // console.log('data', data);
-    console.log(chalk.green(`${username}: ${cmd.split('\n')[0]}`));
-});
+  const { cmd, username } = data
+  // console.log('data', data);
+  console.log(chalk.green(`${username}: ${cmd.split('\n')[0]}`))
+  listenForChat()
+})
+
+// ================================================================
+// Whisper Response
+
+socket.on('whisper', messageObj => {
+  const { message, user } = messageObj
+  console.log(chalk.blue(`Whisper from ${user}: ${message}`))
+  listenForChat()
+})
+
+// ================================================================
+// Toxic Comment Response
 
 socket.on('toxic', data => {
-  const { cmd } = data;
-  console.log(chalk.red(`SERVER: ${cmd.split('\n')[0]}`))  
-});
+  const { cmd } = data
+  console.log(chalk.red(`SERVER: ${cmd.split('\n')[0]}`))
+  listenForChat()
+})
 
-//   repl.start({
-//     prompt: '',
-//     terminal: false,
-//     eval: (cmd) => {
-//       socket.send({cmd, username});
-//     }
-// });
+// ================================================================
+// Invalid Room Error - Triggered by Join Event to Non-Existent Room
 
+socket.on('invalid room', error => {
+  console.log(chalk.red(`SERVER ERROR: ${error.error}`))
+  listenForChat()
+})
+
+// ================================================================
+// Command Switch Block
+
+const handleCommand = (cmd) => {
+  const command = cmd.match(/[a-z]+\b/)[0]
+  const args = cmd.match(/[a-z-]+\b/g)[1]
+  const message = cmd.substr(command.length + args.length + 3, cmd.length)
+  console.log(chalk.red(command), chalk.red(args), chalk.red(message))
+  switch (command) {
+    case 'join': // ie: foobar
+      console.log('join')
+      socket.emit('join', args)
+      break
+    case 'exit': // /exit
+      console.log('exit')
+      socket.emit('disconnected', socket.username)
+      process.exit(0)
+    case 'whisper':
+      console.log('whisper', args, message)
+      socket.emit('whisper', { user: args, message })
+      listenForChat()
+      break
+    default:
+      listenForChat()
+  }
+}
