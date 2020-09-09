2b4958ff067b288376fd880f3b36683c9df1d87f
blakerom <blakeromero001@gmail.com>
2020-09-08 17:28:37 -0700

    end of day commmit refactoring and troubleshooting message functionality

--- index.js
+++ index.js
@@ -25,135 +25,11 @@ const {
   connected,
   messageHandler,
   whisperHandler,
-  toxicHandler
+  toxicHandler,
+  unavailableHandler
 } = handlers.eventhandlers;
-// ================================================================
 
-// ================================================================
-// Disconnect Event
-
-// const loginOrSignup = () => {
-//   inquirer.prompt([login]).then(answers => {
-//     if (answers.answer === 'sign-in') {
-//       inquirer.prompt([signInUsername, signInPassword]).then(user => {
-//         socket.emit('signin', user)
-//       })
-//     } else {
-//       inquirer.prompt([signInUsername, signInPassword]).then(user => {
-//         socket.emit('signup', user)
-//       })
-//     }
-//   })
-// }
-
-// // ================================================================
-// // Listen for Chat Event
-
-// const listenForChat = () => {
-//   inquirer.prompt(message).then(message => {
-//     const cmd = message.chat
-//     if (cmd[0] === '/') {
-//       handleCommand(cmd)
-//     } else if (!cmd.length) {
-//       listenForChat()
-//     } else {
-//       socket.send({ cmd, username: socket.username })
-//       listenForChat()
-//     }
-//   })
-// }
-
-// // ================================================================
-// // Invalid Login Response - Loop back to login/signup prompt
-// const invalidLoginHandler = (error, cb) => {
-//   console.log(chalk.red(`SERVER ERROR: ${error.err}`))
-//   cb()
-// }
-
-// // ================================================================
-// // After Successful Login
-
-// const connected = (username, cb) => {
-//   console.log(chalk.red(`=== start chatting ${username} ===`))
-//   cb()
-// }
-
-// // ================================================================
-// // Normal Message Response
-
-// const messageHandler = (data, cb) => {
-//   const { cmd, username } = data
-//   console.log(chalk.green(`${username}: ${cmd.split('\n')[0]}`))
-//   cb()
-// }
-
-// // ================================================================
-// // Whisper Response
-
-// const whisperHandler = (data, cb) => {
-//   const { message, user } = data
-//   console.log(chalk.blue(`Whisper from ${user}: ${message}`))
-//   cb()
-// }
-
-// // ================================================================
-// // Toxic Comment Response
-
-// const toxicHandler = (data, cb) => {
-//   const { cmd } = data
-//   console.log(chalk.red(`SERVER: ${cmd.split('\n')[0]}`))
-//   cb()
-// }
-
-// // ================================================================
-// // Invalid Room Error - Triggered by Join Event to Non-Existent Room
-
-// const invalidRoomHandler = (error, cb) => {
-//   console.log(chalk.red(`SERVER ERROR: ${error.error}`))
-//   cb()
-// }
-
-// ================================================================
-// Command Switch Block
-
-// const handleCommand = (cmd) => {
-//   const command = cmd.match(/[a-z]+\b/)[0]
-//   const args = cmd.match(/[a-z-]+\b/g)[1]
-//   const message = cmd.substr(command.length + args.length + 3, cmd.length)
-//   switch (command) {
-//     case 'join': // ie: foobar
-//       if(args) {
-//         socket.emit('join', args)
-//       } else {
-//         socket.emit('getrooms')
-//         socket.on('sendrooms', (list) => {
-//           menu.choices = list
-//           inquirer.prompt([menu]).then(choice => {
-//             socket.emit('join', choice)
-//           })
-//         })
-//       }
-//       break
-//     case 'getusers':
-//       socket.emit('getusers')
-//       socket.on('sendusers', (list) => {
-//         users.choices = list;
-//         inquirer.prompt([users, whisper]).then(choice => {
-//           socket.emit('whisper', choice)
-//         })
-//       })
-//     case 'exit': // /exit
-//       socket.emit('disconnected', socket.username)
-//       process.exit(0)
-//     case 'whisper':
-//       socket.emit('whisper', { user: args, message })
-//       listenForChat()
-//       break
-//     default:
-//       listenForChat()
-//   }
-// }
-
+let login = false;
 // ================================================================
 // Socket Listeners
 
@@ -162,7 +38,7 @@ socket.on('disconnect', () => {
 })
 
 socket.on('connect', () => {
-  console.log('here')
+  // console.log('here')
   loginOrSignup()
 })
 
@@ -171,7 +47,9 @@ socket.on('invalid-login', error => {
 })
 
 socket.on('connected', username => {
-  console.log('I heard')
+  // console.log('I heard')
+  login = true;
+  socket.username = username;
   connected(username, listenForChat)
 })
 
@@ -195,6 +73,9 @@ socket.on('invalid room', data => {
   invalidRoomHandler(data, listenForChat)
 })
 
-socket.on('unavailable', () => {
-  // TODO: need to write
+socket.on('unavailable', error => {
+  unavailableHandler(error, listenForChat)
 })
+
+  listenForChat()
+
